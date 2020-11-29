69fc86898e3cc0d860fd41a342356b3ae38754ca
PolarAmina <75171909+PolarAmina@users.noreply.github.com>
2020-11-29 10:16:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(8080);
 console.log("server started");
 
 var SOCKET_LIST = {};
